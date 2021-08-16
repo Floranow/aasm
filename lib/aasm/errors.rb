bc8590cbe272efc5e43076189b68d9f8aef75cda
abdulmughniHamzah <abdulmughni.h@gmail.com>
2021-08-16 17:53:12 +0300

    message

--- lib/aasm/errors.rb
+++ lib/aasm/errors.rb
@@ -6,18 +6,17 @@ module AASM
     attr_reader :object, :event_name, :originating_state, :failures, :state_machine_name
 
     def initialize(object, event_name, state_machine_name, failures = [], custom_message = nil)
-      @object, @event_name, @originating_state, @failures = object, event_name, object.aasm(state_machine_name).current_state, failures
+      @object, @event_name, @originating_state, @failures, @custom_message = object, event_name, object.aasm(state_machine_name).current_state, failures, custom_message
       @state_machine_name = state_machine_name
-      if custom_message
-        super(custom_message)
-      else
-        super("Event '#{event_name}' cannot transition from '#{originating_state}'.#{reasoning}")
-      end
+      super("Event '#{event_name}' cannot transition from '#{originating_state}'.#{reasoning}") unless custom_message.present?
 
     end
 
     def reasoning
       " Failed callback(s): #{failures}." unless failures.empty?
+    end
+    def message
+      @custom_message
     end
   end
 
